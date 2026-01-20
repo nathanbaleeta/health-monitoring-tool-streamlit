5f1b53220d9c067cd4ea9a65d103f682017942c0
nathanbaleeta <nbaleeta@gmail.com>
2026-01-20 14:36:00 +0300

    add interactive filter using select box

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Function to fetch data from the API
 @st.cache_data # Cache the data to prevent re-fetching on every rerun
-def fetch_api_data(url):
+def get_health_stats_data(url):
     try:
         response = requests.get(url)
         # Raise an exception for bad status codes
@@ -23,7 +23,7 @@ st.write("A public health monitoring platform tool that collects and visualizes 
 API_ENDPOINT = "https://disease.sh/v3/covid-19/countries"
 
 # Fetch data
-api_data = fetch_api_data(API_ENDPOINT)
+api_data = get_health_stats_data(API_ENDPOINT)
 
 if api_data is not None:
     # Convert the fetched list of dictionaries to a pandas DataFrame
@@ -32,11 +32,20 @@ if api_data is not None:
     # Drop unnecessary columns
     df.drop('countryInfo', inplace=True, axis=1)
 
+    # Create a selectbox with unique categories
+    country_selection = st.selectbox(
+        'Select Country',
+        options=df['country'].unique()
+    )
+
+    # Filter the data based on the selection
+    filtered_df = df[df['country'] == country_selection]
+
     st.subheader("Data from API.")
-    st.write(f"Fetched {len(api_data)} records.")
+    st.write(f"Fetched {len(filtered_df)} records.")
 
     # Display the DataFrame in an interactive table
-    st.dataframe(df) # Use st.dataframe for an interactive table
+    st.dataframe(filtered_df) # Use st.dataframe for an interactive table
 
 else:
     st.warning("Could not load data from the API.")
