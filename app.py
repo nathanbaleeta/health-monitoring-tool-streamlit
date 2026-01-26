a282efbbb505e14376d0a858d4b7d9ccc728e5f4
nathanbaleeta <nbaleeta@gmail.com>
2026-01-26 12:54:11 +0300

    added streamlit-dynamic-filters to sidebar

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+
 import streamlit as st
-import requests
 import pandas as pd
+import numpy as np
+import requests
 from streamlit_dynamic_filters import DynamicFilters
 
+st.set_page_config(
+    # Title and icon for the browser's tab bar:
+    page_title="Seattle Weather",
+    page_icon="🌦️",
+    # Make the content take up the width of the page:
+    layout="wide",
+)
 
-# Streamlit app layout
 st.title("Health Monitoring Tool")
 st.write("A public health monitoring platform tool that collects and visualizes data related to community health.")
 
@@ -24,9 +32,7 @@ def get_health_stats_data(url):
     except requests.exceptions.RequestException as e:
         st.error(f"Error fetching data: {e}")
         return None
-
-
-
+    
 # Fetch data
 api_data = get_health_stats_data(API_ENDPOINT)
 
@@ -40,15 +46,8 @@ if api_data is not None:
     # Initialize dynamic filters
     dynamic_filters = DynamicFilters(df, filters=['continent', 'country'])
 
-    # Display filters and the filtered dataframe
-    dynamic_filters.display_filters()
+    # Display filters in sidebar and the filtered dataframe in main pane
+    dynamic_filters.display_filters(location='sidebar')
     dynamic_filters.display_df()
-
-    
-
-
-    
-
 else:
     st.warning("Could not load data from the API.")
-
